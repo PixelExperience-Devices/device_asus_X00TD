bf5c63841a114894ab7f1f59ddd67289838339c9
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    X00TD: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
