a51d74578f6fbd7af5454bc7fb96edd6e1ec9ec6
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    X00TD: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
