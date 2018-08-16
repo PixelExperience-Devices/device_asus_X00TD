3c64f963c6fc30e7a05f5065ad0afe9238b0c67d
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    X00TD: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
