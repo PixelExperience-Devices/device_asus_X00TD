d4c30d00750de46a20230c025eb8fb00c8a3ce93
Bruno Martins <bgcngm@gmail.com>
2018-10-02 21:57:25 +0100

    X00TD: Ship additional textclassifier's smart selection models
    Change-Id: I4de9326fe60a395454834906552518afbc376f49
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
