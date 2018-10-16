484aad2f71bf4d689d7040f47099695b17e34256
Daniel Mentz <danielmentz@google.com>
2018-10-16 11:17:47 -0700

    X00TD: Remove ineffective BOARD_KERNEL_TAGS_OFFSET variable
    Remove various variables related to where kernel and ramdisk are loaded
    by the bootloader. There are two reasons for this:
    
    1. The B1C1 bootloader ignores all the offset values for kernel, ramdisk
    and tags. The default B1C1 memory layout has been designed to accomodate
    a larger KASAN kernel.
    
    2. The variables BOARD_KERNEL_TAGS_OFFSET and BOARD_RAMDISK_OFFSET are
    ignored by the Android build system (unless they are copied into the
    BOARD_MKBOOTIMG_ARGS variable). This is in contrast to the variables
    BOARD_KERNEL_BASE, BOARD_KERNEL_PAGESIZE and BOARD_MKBOOTIMG_ARGS which
    are referenced by build/make/core/Makefile
    
    Bug: 117832711
    Change-Id: I3404a4e084ed063d51077a3a80229854362c768b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,8 +80,6 @@ BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 loop.max_part=7
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 
 TARGET_KERNEL_CONFIG := X00TD_defconfig
