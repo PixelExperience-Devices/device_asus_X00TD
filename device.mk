5bc1ff33c7296797fefaacf4126cae25c7b32bd9
Jeff Tinker <jtinker@google.com>
2018-11-05 15:04:17 -0800

    X00TD: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug: 117570956
    bug: 110838441
    bug: 116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705

--- device.mk
+++ device.mk
@@ -94,7 +94,8 @@ TARGET_SCREEN_WIDTH := 1080
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
