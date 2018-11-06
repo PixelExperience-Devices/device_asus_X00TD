b38dc1b835ed8f1721ed46f2f79429381135485e
allianceSparx <alliancesparx@gmail.com>
2018-11-06 23:14:56 +0000

    X00T: Enable USB debugging at boot
    Change-Id: Ie827a6d77473489b04d138c1d24a5ef123f89326

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -173,3 +173,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# USB debugging at boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
