c7cd2e47b8ae1e6d34525686354e622e2a9edd9b
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    X00TD: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl
 
