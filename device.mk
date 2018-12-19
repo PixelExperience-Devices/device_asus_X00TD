b12766128dfb5317f17ec0b92d32e3caec0ef0bb
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 21:57:31 +0530

    X00TD: Build telephony-ext
    Change-Id: I2dd7d94ba00af4d20aa2410b7c5ee54726a6ed59

--- device.mk
+++ device.mk
@@ -219,6 +219,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
