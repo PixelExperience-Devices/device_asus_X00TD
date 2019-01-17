8f44577a519e2a7c24456ff8cc416602a54c039c
Subinsmani <subins.mani@gmail.com>
2019-01-17 15:37:04 +0530

    X00TD: Add Google Camera

--- device.mk
+++ device.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap
+    GoogleCamera
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
