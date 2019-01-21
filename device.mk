9d128ec70327ca31bb13268a32f07413617fece4
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    X00TD: Build livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -191,6 +191,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_sdm660
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl
