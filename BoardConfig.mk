1aadc7edfe7f6cdd2772223149c41c7a6f2c45c5
celtare21 <celtare21@gmail.com>
2019-01-24 11:21:07 +0000

    X00T: BoardConfig: Properly set dexpreopt
    Change-Id: Icb2e129000676938b75b94c1279b8df676a4ed21
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,12 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_DEBUG_INFO := false
+      USE_DEX2OAT_DEBUG := false
+      DONT_DEXPREOPT_PREBUILTS := true
+      WITH_DEXPREOPT_PIC := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
   endif
 endif
 
