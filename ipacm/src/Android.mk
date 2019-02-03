187add92b476a9796b4a9c02db664614e2125d58
Ghanim Fodi <gfodi@codeaurora.org>
2019-02-03 12:07:23 +0200

    ipacm: Add kona target to IPAv3 target list
    Add kona target to IPAv3 target list as kona
    uses IPAv3 driver.
    
    Change-Id: I75091e46d90e3489372a122a15f92649ed5404e8

--- ipacm/src/Android.mk
+++ ipacm/src/Android.mk
@@ -5,6 +5,7 @@ BOARD_IPAv3_LIST := msm8998
 BOARD_IPAv3_LIST += sdm845
 BOARD_IPAv3_LIST += sdm710
 BOARD_IPAv3_LIST += msmnile
+BOARD_IPAv3_LIST += kona
 BOARD_IPAv3_LIST += $(MSMSTEPPE)
 
 ifneq ($(call is-board-platform-in-list,$(BOARD_PLATFORM_LIST)),true)
