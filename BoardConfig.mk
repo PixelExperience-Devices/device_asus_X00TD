2cf865418aa624275fd57132a71df6f8d1fb2f00
Roshan Pius <rpius@google.com>
2019-03-06 11:23:18 -0800

    X00TD: Turn off SoftAP MAC randomization
    Device does not seem to support MAC reconfiguration.
    
    Bug: 127559063
    Test: Manual verification of toggling softap on/off.
    Change-Id: I9ae25b4d14a0c207188fae2c52ac7791b2061a40
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -276,6 +276,7 @@ WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # inherit from the proprietary version
 -include vendor/asus/X00TD/BoardConfigVendor.mk
