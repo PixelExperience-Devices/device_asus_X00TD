661e04cb64a4ba154428c9ca770b4709bf045e35
anomalchik <vidslava@yandex.ru>
2019-03-19 14:23:51 -0100

    X00T: rcs service is no longer used. Clean it
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- device.mk
+++ device.mk
@@ -352,13 +352,6 @@ PRODUCT_PACKAGES += \
     move_time_data.sh \
     move_wifi_data.sh
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
