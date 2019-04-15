b0d076476e578b218789efb981c15a323d1d9c98
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    X00TD: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
