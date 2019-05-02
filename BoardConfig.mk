39f4ccfeaeb2216756fb9b6172193e2b64c7b97e
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-05-02 09:55:17 +0700

    X00T: add back liblight
    * from LA.UM.7.2.r1-06700-sdm660.0
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,9 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Lineage Hardware
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
