086582e8495dbe54f63fd2229c2aecccc76fd6ad
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-06 18:08:17 +0200

    X00TD: build libbthost_if from hardware/qcom
    Change-Id: Ifbaf499a0825c288c642486db455c9350f45fe32
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbthost_if
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
