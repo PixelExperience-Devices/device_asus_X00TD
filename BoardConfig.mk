f8ae89461a24e1cdb0cc88d3198cf8ee7c424467
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    X00TD: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_VNDK_VERSION := current
