5ec32914f5ec78a9f018cc861f549e3c33fc1d98
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-06-02 19:32:20 +0700

    X00T: init: remove check for other device beside global
    because this device tree mainly used for only my phone,
    so there's no point check for other.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- init/init_X00T.cpp
+++ init/init_X00T.cpp
@@ -103,56 +103,18 @@ static void init_alarm_boot_properties()
 
 void vendor_check_variant()
 {
-    struct sysinfo sys;
-    char const *region_file = "/mnt/vendor/persist/flag/countrycode.txt";
     char const *build_fingerprint, *product_device, *product_model, *product_name;
     std::string region;
 
-    sysinfo(&sys);
+    // Product name overrides
+    product_name = "WW_X00TD";
 
-    // Make sure the region value is trimmed first
-    if (ReadFileToString(region_file, &region))
-        region = Trim(region);
-
-    // Russian model has a slightly different product name
-    if (region == "RU")
-        product_name = "RU_X00TD";
-    else
-        product_name = "WW_X00TD";
-
-    // 6 GB variant
-    if (sys.totalram > 4096ull * 1024 * 1024) {
-        // Russian model
-        if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_9:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_9";
-
-        // Global model
-        } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_3";
-        }
-
-    // 3/4 GB variants
-    } else {
-        // Russian model
-        if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_6:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_6";
-
-        // Global model
-        } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
-            product_device = "ASUS_X00T_2";
-        }
-    }
+    // build fingerprint
+    build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1905.053-20190513:user/release-keys";
+    product_device = "ASUS_X00T_2";
 
     // Product model overrides
-    if (region == "RU" || region == "TW" ||
-        (region == "PH" && sys.totalram > 3072ull * 1024 * 1024))
-        product_model = "ASUS_X00TDB";
-    else
-        product_model = "ASUS_X00TD";
+    product_model = "ASUS_X00TD";
 
     // Override props based on values set
     property_override_dual("ro.product.device", "ro.vendor.product.device", product_device);
@@ -161,6 +123,7 @@ void vendor_check_variant()
     property_override_triple("ro.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", build_fingerprint);
 
     // Set region code via ro.config.versatility prop
+    region = "ID";
     property_set("ro.config.versatility", region);
 }
 
