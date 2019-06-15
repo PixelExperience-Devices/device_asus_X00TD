551ca63701533580b3d5069702c7ade661da330a
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-06-15 10:08:44 +0700

    X00T: update cpuset
    * foreground use cpu 0-3 and cpu 7
    * background use 2-3
    * system-background use 0-3
    * restricted 0-3
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2400,8 +2400,8 @@ case "$target" in
             echo 20 > /proc/sys/kernel/sched_small_wakee_task_load
 
             # cpuset settings
-            echo 0-3 > /dev/cpuset/background/cpus
-            echo 0-3 > /dev/cpuset/system-background/cpus
+            # echo 0-3 > /dev/cpuset/background/cpus
+            # echo 0-3 > /dev/cpuset/system-background/cpus
 
             # disable thermal bcl hotplug to switch governor
             echo 0 > /sys/module/msm_thermal/core_control/enabled
