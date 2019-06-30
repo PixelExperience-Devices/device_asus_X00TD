416f883dd81353776119b454acf691cfbb2e99fa
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-07-01 03:59:50 +0530

    X00TD: rootdir: Cleanup unused init scripts
    Change-Id: Ic23539993f356dbac367cc118655054305638572

--- device.mk
+++ device.mk
@@ -338,12 +338,10 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.qseecomd.sh
+    init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
