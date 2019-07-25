aa2e225ad4928147d404e69c9f7c3c86ab70d85b
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-07-25 10:32:39 +0700

    X00T: build fingerprint biometrics from source
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- device.mk
+++ device.mk
@@ -178,7 +178,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    fingerprintd
+    android.hardware.biometrics.fingerprint@2.1-service-X00T
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
