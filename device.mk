d8baecb418105b5f5690de31829c5360400a05c1
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-08-02 06:21:38 +0700

    X00T: build vendor.display.config@1.3
    liboemcrypto.so need vendor.display.config@1.3
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- device.mk
+++ device.mk
@@ -154,7 +154,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
     copybit.sdm660 \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
@@ -163,8 +165,10 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libvulkan \
     libtinyxml \
+    libgenlock \
     memtrack.sdm660 \
-    libgenlock
+    vendor.display.config@1.3 \
+    vendor.display.config@1.3.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
