8fff25fb5184fa01a96a0c9d8959af33730c03dc
Dhananjay Kumar <dhakumar@codeaurora.org>
2019-08-06 16:31:51 +0530

    X00TD: audio: update properties to fix namespace violations
    Update properties to add vendor.audio prefix where missing
    and remove unused ones to fix namespace violations.
    
    Change-Id: I3723ae3e366178e87eff81001a0f1fd133cc4404
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -150,7 +150,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #enable headset calibration
 PRODUCT_PROPERTY_OVERRIDES += \
-audio.volume.headset.gain.depcal=true
+vendor.audio.volume.headset.gain.depcal=true
 
 #enable dualmic fluence for voice communication
 PRODUCT_PROPERTY_OVERRIDES += \
