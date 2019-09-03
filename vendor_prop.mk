5ad295c6675dd414cac67b09b3441a945790ee65
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 03:20:57 +0430

    X00TD: Force disable bluetooth audio hal 2.0
    Change-Id: I217becb041395865a1c6f997b0cbc0c994d04e78
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -50,7 +50,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.splita2dp=true \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true \
-    vendor.qcom.bluetooth.soc=cherokee
+    vendor.qcom.bluetooth.soc=cherokee \
+    persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
