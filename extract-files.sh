fc41b16f87c0094cc3f82c9216795f385df46233
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-09 19:28:04 +0530

    X00TD: patch libMiWatermark to load older icuuc and minikin
    E HAL : dlopen failed: cannot locate symbol "_ZN6icu_6011StringPieceC1EPKc" referenced by "/vendor/lib/libMiWatermark.so"
    E HAL : dlopen failed: cannot locate symbol "_ZNK7minikin6Layout10getAdvanceEv" referenced by "/vendor/lib/libMiWatermark.so"
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,11 @@ patchelf --add-needed libui-v28.so "$MOT_GPU_MAPPER"
 
 . "$MY_DIR"/setup-makefiles.sh
 
+patchelf --replace-needed "libicuuc.so" "libicuuq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libMiWatermark.so
+patchelf --replace-needed "libminikin.so" "libminiq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libMiWatermark.so
+patchelf --set-soname "libicuuq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libicuuc.so
+patchelf --set-soname "libminiq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libminikin.so
+
 # Load camera.sdm660.so shim
 CAM_SDM660="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
 patchelf --add-needed camera.sdm660_shim.so "$CAM_SDM660"
