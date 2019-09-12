076e6d0b4dbeec9fac65fbbdb420952d8351673e
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-13 00:17:36 +0200

    X00TD: Dexpreopt everything
    Change-Id: I65e8ce61e33256b2f2beecf390f6926b70920d02
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,9 +166,11 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-  endif
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+        endif
+    endif
 endif
 
 # Filesystem
