008b417cfd97491cd726cfec561c431b5b3e6690
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    X00TD: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -259,6 +259,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+# HW Crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti
+
 # IMS
 PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
