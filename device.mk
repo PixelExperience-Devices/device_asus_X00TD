6ad6a227fbd51f307bf6c68fb7f2681bba8d2d4a
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    X00TD: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -68,6 +68,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libbthost_if
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
