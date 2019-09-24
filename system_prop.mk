a0ce528fbbfe7cfd9b6465ce0fa288de408f7044
Vasantha Balla <vballa@codeaurora.org>
2019-09-24 12:39:06 +0530

    X00TD: Add property to prefer hw codecs in thumbnail
    Add property "media.stagefright.thumbnail.prefer_hw_codecs"
    to system.prop
    
    Change-Id: If60f9cdc8b0236df421a72e7621f1e57fd3f4f55
    Signed-off-by: Subinsmani <subins.mani@gmail.com>
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -3,3 +3,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase.ms=android-asus-tpin \
     ro.com.google.rlzbrandcode=ASUP \
     ro.com.google.rlz_ap_whitelist=y0,y5,y6,y7,y9
+
+#prefer HW codec over SW for thumbnail
+media.stagefright.thumbnail.prefer_hw_codecs=true
