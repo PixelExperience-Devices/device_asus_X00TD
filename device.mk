5f9099f03429b7fb9d9aa6cc82f52f342615a4d7
Bruno Martins <bgcngm@gmail.com>
2019-09-30 13:32:04 +0100

    X00TD: Drop mediaextractor seccomp policy
    These syscalls are now allowed by default.
    
    Change-Id: I405f609d030331e19031612cbd912a54a3c72339

--- device.mk
+++ device.mk
@@ -174,8 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/configs/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # USB
 PRODUCT_PACKAGES += \
