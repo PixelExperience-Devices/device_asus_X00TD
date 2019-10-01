b2c71d44f9956ad2d15bf3b38be97aa86b96b40d
Ritesh Saxena <riteshsax007@gmail.com>
2019-10-01 14:19:39 +0000

    X00TD: update cne dpm from davinci

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -80,6 +80,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=true
 
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=1
+    persist.vendor.dpm.nsrm.bkg.evt=3955
+
 # Dalvik overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
