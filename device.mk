bfd5944c20e91cf902b13ae7c1b1a0145d16e3c0
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-10-31 15:58:15 +0700

    X00T: move PRODUCT_DEXPREOPT_SPEED_APPS to device.mk
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- device.mk
+++ device.mk
@@ -329,6 +329,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Pre-opt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
