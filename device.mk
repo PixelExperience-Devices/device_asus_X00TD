25805fa6f78cf98028800d67fd8b9b31a4a6724d
GiaSen <giammasena@gmail.com>
2019-11-01 22:52:41 +0530

    X00TD: Uprev power hal to 1.2
    Signed-off-by: bablusss <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -374,7 +374,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
