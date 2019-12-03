dd5386c977cc5a459c61acd8ff3dd68b88f7a433
Rizky Benggolo <travarilo@gmail.com>
2019-12-03 12:50:48 +0000

    X00T: Revert to old fingerprint biometrics
    Change-Id: I320efb57374a956bdc60c2dfbb1897aefbdece7d

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-X00T
+    fingerprintd
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
