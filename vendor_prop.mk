ca895ecd9a79493804a086f482ef8a517b7dd528
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-12-11 03:42:27 +0700

    X00T: update dalvik.vm values
    with recent commit on high-mem-java-heap-config, values based on between
    4gb and 2gb values
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -66,8 +66,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heaptargetutilization=0.70 \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
 
 # DRM
