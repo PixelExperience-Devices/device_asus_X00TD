f39e0110101a3d9288c19e5bbab0188f39198ec9
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-12-12 01:40:23 +0530

    X00T: Build vendor.qti.hardware.bluetooth_audio@2.0 from source
    Change-Id: I53f8c916452d30a0de6645f1ddf02aa6de358b78

--- device.mk
+++ device.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
