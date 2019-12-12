dc54b69a290353a3b45b2ba1ac7955ede471cec9
Rizky Benggolo <travarilo@gmail.com>
2019-12-12 08:14:55 +0000

    X00T: update build fingerprint to 16.2017.1910.059
    Change-Id: I76d2221e76debace5551b14b38bc22c1336f31fa

--- init/init_X00T.cpp
+++ init/init_X00T.cpp
@@ -81,7 +81,7 @@ void vendor_check_variant()
     product_name = "WW_X00TD";
 
     // build fingerprint
-    build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1906.055-20190703:user/release-keys";
+    build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1910.059-20190920:user/release-keys";
     product_device = "ASUS_X00T_2";
 
     // Product model overrides
