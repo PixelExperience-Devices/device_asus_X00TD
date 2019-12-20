repo-x00td/android_device_asus_X00T

X00T: HACK: display correct baseband version

device.mk
@@ -331,6 +331,7 @@ PRODUCT_PACKAGES += \
     init.qti.ims.sh \
     move_time_data.sh \
     move_wifi_data.sh \
+    fix_baseband.sh \
     fstab.qcom \
     init.msm.usb.configfs.rc \
     init.qcom.rc \

rootdir/Android.mk
@@ -75,6 +75,14 @@ LOCAL_SRC_FILES    := bin/move_wifi_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fix_baseband.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/fix_baseband.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # etc
 
 include $(CLEAR_VARS)

PixelExperience-Devices/device_asus_X00TD

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
Change-Id: I8457ece0000a60a307f928874f224a4ed2bfd10a

device.mk
@@ -350,7 +350,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.qti.ims.sh \
     move_time_data.sh \
-    move_wifi_data.sh
+    move_wifi_data.sh \
+    fix_baseband.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

rootdir/Android.mk
@@ -83,6 +83,14 @@ LOCAL_SRC_FILES    := bin/init.qcom.ipastart.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fix_baseband.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/fix_baseband.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # etc
 
 include $(CLEAR_VARS)