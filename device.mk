d1e3ab8d70f6ac365bbb26f75a1eac3ace3e56cc
Rizky Benggolo <travarilo@gmail.com>
2019-12-24 14:43:39 +0000

    X00T: Build missing gps libs
    Change-Id: Ib338aadc502156b9e18dcc0c7aed42385563b59b

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 # GPS
+PRODUCT_PACKAGES += \
+    libcurl \
+    libsensorndkbridge
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
