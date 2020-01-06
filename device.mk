LineageOS/android_device_asus_X00TD

X00TD: add touch hal hidl for gestures
Edits:
 - adapt code to work with single node
 - add all supported gestures
 - add custom gesture strings
 - set custom default actions for each gesture

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

device.mk
@@ -442,6 +442,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00TD
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service

PixelExperience-Devices/device_asus_X00TD

device.mk
@@ -444,6 +444,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00T
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service