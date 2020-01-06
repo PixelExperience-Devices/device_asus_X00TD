6ce90a952a3edc6c2513addba2f161edf7d070fd
Saurabh Charde <saurabhchardereal@gmail.com>
2020-01-06 22:38:48 +0530

    X00TD: libinit: Set dalvik-heap configurations based on RAM variant
    * Since we have 3 models with different amount of RAM
    
    * Values taken from frameworks/native
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -72,6 +72,38 @@ void property_override_triple(char const system_prop[], char const vendor_prop[]
     property_override(bootimg_prop, value);
 }
 
+void dalvik_properties()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram > 4096ull * 1024 * 1024) {
+        // Set for 6GB or more RAM
+        property_set("dalvik.vm.heapstartsize", "16m");
+        property_set("dalvik.vm.heapgrowthlimit", "256m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.5");
+        property_set("dalvik.vm.heapmaxfree", "32m");
+        property_set("dalvik.vm.heapminfree", "8m");
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // Set for 4GB RAM
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.6");
+        property_set("dalvik.vm.heapmaxfree", "16m");
+        property_set("dalvik.vm.heapminfree", "8m");
+    } else {
+        // Set for 2/3GB RAM
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapmaxfree", "8m");
+        property_set("dalvik.vm.heapminfree", "512k");
+  }
+}
+
 void vendor_check_variant()
 {
     struct sysinfo sys;
@@ -130,4 +162,5 @@ void vendor_check_variant()
 void vendor_load_properties()
 {
     vendor_check_variant();
+    dalvik_properties();
 }
