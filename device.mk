7eda830e6b76b2d78335d9c2fb0ce1f76b12ab60
lucky11295 <56794206+lucky11295@users.noreply.github.com>
2020-01-23 09:07:58 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -370,8 +370,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
 
 # Power
 PRODUCT_PACKAGES += \
