1f0de42e5c85f7426ea37c3e401179b89eeca824
Rizky Benggolo <travarilo@gmail.com>
2020-02-08 17:17:01 +0700

    X00T: Use crosshatch build fingerprint

--- init/init_X00T.cpp
+++ init/init_X00T.cpp
@@ -95,12 +95,12 @@ void vendor_check_variant()
     if (sys.totalram > 4096ull * 1024 * 1024) {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_9:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
+            build_fingerprint = "google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys";
             product_device = "ASUS_X00T_9";
 
         // Global model
         } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_3:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
+            build_fingerprint = "google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys";
             product_device = "ASUS_X00T_3";
         }
 
@@ -108,12 +108,12 @@ void vendor_check_variant()
     } else {
         // Russian model
         if (region == "RU") {
-            build_fingerprint = "asus/RU_X00TD/ASUS_X00T_6:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
+            build_fingerprint = "google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys";
             product_device = "ASUS_X00T_6";
 
         // Global model
         } else {
-            build_fingerprint = "asus/WW_X00TD/ASUS_X00T_2:9/PKQ1/16.2017.1912.060-20191125:user/release-keys";
+            build_fingerprint = "google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys";
             product_device = "ASUS_X00T_2";
         }
     }
--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -37,10 +37,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1455 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ1A.200205.002 6084386 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1912.060-20191125:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys
 
 # The following system and vendor props will be set by vendor init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
