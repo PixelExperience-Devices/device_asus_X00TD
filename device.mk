7d2496200eae9e2416b6ed1530b1b5ad86db5e04
Rizky Benggolo <travarilo@gmail.com>
2020-02-17 11:50:57 +0700

    X00T: Build vendor.qti.hardware.camera.device@1.0
    Change-Id: If78e824a4049bf166926cf2149e07136f4c28d9f

--- device.mk
+++ device.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libxml2 \
     Snap
 
