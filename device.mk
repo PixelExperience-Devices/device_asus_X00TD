c21137f4fa4c6a8714c2839915b0b04ac5230202
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:39:35 -0800

    X00TD: Remove unnecessary android.hardware.radio entries
    * These are implicitly included by AOSP.
    
    Change-Id: I9e557444161e7ca60793bddde82330a6bd04908b
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -359,8 +359,6 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.2 \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
