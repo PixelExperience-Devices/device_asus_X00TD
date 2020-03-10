ea8f8630255047d8390200801b17f43b21487c97
alexmercer09 <supremegod.of.war@gmail.com>
2020-03-10 17:08:12 +0000

    Initial Changes for pe
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/aosp_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-user \
-    lineage_X00T-userdebug \
-    lineage_X00T-eng
+    aosp_X00T-user \
+    aosp_X00T-userdebug \
+    aosp_X00T-eng
