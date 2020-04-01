46b936e42a3daf3ef7432eae207ed6dc7ec322b3
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-04-01 20:08:51 +0200

    X00T: Bind mount and disable acdb QRD calibration configs
    * Improves audio greatly!
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: I952d8182b924621577ad6b2e4cf5905cc1fa1000

--- device.mk
+++ device.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# Custom init
+PRODUCT_PACKAGES += \
+    init.qcom.rc
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
