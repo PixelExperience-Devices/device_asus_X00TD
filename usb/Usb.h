73a8e974bffbc0f428e777eced9759ba603cb38a
alexmercer09 <supremegod.of.war@gmail.com>
2020-04-05 13:45:57 +0000

    X00T > X00TD
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

--- usb/Usb.h
+++ usb/Usb.h
@@ -10,7 +10,7 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service.X00T"
+#define LOG_TAG "android.hardware.usb@1.0-service.X00TD"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
