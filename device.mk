f30e770b14c07b0d987281188a62bd3698e73712
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-04-21 22:20:44 +0000

    X00TD: rootdir: Clean-up vendor init scripts
    Change-Id: Ia6a262f67fc5310a06adcd4b1c887d05a2474c8d

--- device.mk
+++ device.mk
@@ -376,9 +376,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.btmac.sh \
-    init.class_main.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh
 
