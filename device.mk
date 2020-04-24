131f146978e6d857228282fe928f01573fa41d9c
alexmercer09 <supremegod.of.wr@gmail.com>
2020-04-24 17:40:20 +0000

    X00TD: Add Zenmotions
    - Based on Zenmotions implementation by Sagar Makhar
    - Migrate to androidx library
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>
    
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -535,8 +535,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Zen Parts
-#PRODUCT_PACKAGES += \
-#    ZenParts
+# Zenmotions
+PRODUCT_PACKAGES += \
+    Zenmotions
 
 $(call inherit-product, vendor/asus/X00TD/X00TD-vendor.mk)
