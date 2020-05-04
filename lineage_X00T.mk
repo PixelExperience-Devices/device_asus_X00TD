bb6feccad8b0d1bb7919d386c66f779b3a2a4b91
Rizky Benggolo <travarilo@gmail.com>
2020-05-05 02:37:07 +0700

    X00T: Update coral build fingerprint to QQ2A.200501.001.B2
    Change-Id: I39adfa21863baab95269b1ee4f74f651aabb4334

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -37,10 +37,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200405.005 6254899 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200501.001.B2 6352890 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys
 
 # The following system and vendor props will be set by vendor init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
