da36ce2e11754fea907d6ed85e341c9f8ce9725e
alexmercer09 <supremegod.of.war@gmail.com>
2020-05-05 15:50:11 +0000

    X00TD: add prebuilt Gcam Go
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

---
+++ prebuilts/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraGo
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/GoogleCameraGo/GoogleCameraGo.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Camera2 Snap Snap2
+include $(BUILD_PREBUILT)
