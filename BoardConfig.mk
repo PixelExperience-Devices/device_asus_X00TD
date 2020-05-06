88ba97df8fd912589f3633c98495d5a5ff41b81e
alexmercer09 <supremegod.of.war@gmail.com>
2020-05-06 12:25:43 +0530

    X00TD: Update vendor security patch *from latest stock android 10 update
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,7 +222,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-12-01
+VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
