ed05d6921d2d27ea283cb1b04e0fbd2999bb8128
alexmercer09 <supremegod.of.war@gmail.com>
2020-05-16 16:45:04 +0000

    X00TD: dont build Zenmotions
    Signed-off-by: alexmercer09 <supremegod.of.war@gmail.com>

--- device.mk
+++ device.mk
@@ -534,8 +534,5 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Zenmotions
-PRODUCT_PACKAGES += \
-    Zenmotions
 
 $(call inherit-product, vendor/asus/X00T/X00T-vendor.mk)
