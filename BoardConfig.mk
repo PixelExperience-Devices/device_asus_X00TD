24eebe15faa108f81c17fac5af0816ec72d02b01
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 18:48:46 +0530

    X00TD: Create vendor.prop
    * Sorted from stock /vendor/build.prop
    
    Change-Id: I988d8d54ec46aa13659caf9a31431cfbec1a8592

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
