985904ff2f280bff0145baf25179207fe377775d
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 22:13:48 +0530

    X00TD: Build Snapcam and camera hidl hals
    Change-Id: Ic87594e946c41413e8bfe5e7c3c37b67b0fed28a
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -94,6 +94,17 @@ PRODUCT_PACKAGES += \
     BluetoothQti \
     libbthost_if
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
