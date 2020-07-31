cb07dcd90e70345f1775689f4751e60b4a9ff96b
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-31 15:46:14 +0530

    X00TD: Explictly build vendor.qti.hardware.camera.device@1.0
    * 64-bit variant isn't being built by default
    
    Change-Id: I75854d887464de063d1bf031c065cc1cc6c6c9b8

--- device.mk
+++ device.mk
@@ -100,7 +100,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5
+    android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0
 
 # CNE
 PRODUCT_PACKAGES += \
