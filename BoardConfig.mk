b30121a4667ab255049d4b9497fbf6817aaac853
Rahul Krishna <rahulkrishna585@gmail.com>
2020-08-30 19:32:16 +0000

    X00TD: Go enforcing
    Change-Id: Ia6766372a427e46e2f97461f2dee609a4adcf52f
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,7 +101,7 @@ BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qc
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
