24a3738af6c1fac22866cb45bb36fab3a1cc8aed
Rizky Benggolo <travarilo@gmail.com>
2020-09-14 16:51:14 +0700

    Revert "X00T: gps: Merge tag 'LA.UM.8.2.r1-06900-sdm660.0'"
    This reverts commit 7196645b3d7833c8c3dcfed511066d2cbd8cea33.

--- gps/android/2.0/location_api/GnssAPIClient.h
+++ gps/android/2.0/location_api/GnssAPIClient.h
@@ -58,7 +58,6 @@ public:
     void gnssUpdateCallbacks(const sp<V1_0::IGnssCallback>& gpsCb,
             const sp<V1_0::IGnssNiCallback>& niCb);
     void gnssUpdateCallbacks_2_0(const sp<V2_0::IGnssCallback>& gpsCb);
-    void gnssUpdateFlpCallbacks();
     bool gnssStart();
     bool gnssStop();
     bool gnssSetPositionMode(V1_0::IGnss::GnssPositionMode mode,
@@ -95,7 +94,6 @@ public:
 
 private:
     void setCallbacks();
-    void setFlpCallbacks();
     void initLocationOptions();
     sp<V1_0::IGnssCallback> mGnssCbIface;
     sp<V1_0::IGnssNiCallback> mGnssNiCbIface;
