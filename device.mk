3fe3fdfd0560c358105128b073d8684d87f92011
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-09-20 21:36:12 +0530

    X00TD: Update NFC configs
    * from UL-ASUS_X01BD-WW-17.2018.2004.424-user build
    * Uprev nfc hal
    
    Change-Id: Ic96e04c05ba230a96bc8e1b042faf1b0d5ded5db

--- device.mk
+++ device.mk
@@ -257,7 +257,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service
+    android.hardware.nfc@1.2-service
 
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
