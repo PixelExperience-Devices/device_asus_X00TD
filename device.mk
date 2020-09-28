39aa97ac5a315db789a8a988129ddbed29e33dac
Rahul Krishna <rahulkrishna585@gmail.com>
2020-09-28 22:38:40 +0530

    X00TD: Build QTI thermal 2.0 hal
    Change-Id: I4d66a57a1fa162c1018ef5d13dee3346cf157a98

--- device.mk
+++ device.mk
@@ -450,9 +450,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm660
+    android.hardware.thermal@2.0-service.qti
 
 # Thermal configs
 PRODUCT_COPY_FILES += \
