943304840b55138a9d81326cecc2a745982cc708
Erfan Abdi <erfangplus@gmail.com>
2020-11-11 22:40:10 +0330

    X00TD: Fix typo in console kernel parameter
    Change-Id: Ia38914588a0b0e3bc30c51df9a7b8681f5bf9d76

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,7 @@ BOARD_HAS_QCA_FM_SOC := cherokee
 BOARD_HAVE_QCOM_FM := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
