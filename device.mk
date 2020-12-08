6d6ee3dad4f4dd7c26cbcbe1e77a972201f82825
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    X00TD: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- device.mk
+++ device.mk
@@ -189,8 +189,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
