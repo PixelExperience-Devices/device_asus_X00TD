3a1dbf1eb00d78925c5dee6bc6ee5bc6df389922
Bharat Pawar <bpawar@codeaurora.org>
2021-01-19 19:49:14 +0530

    X00TD: Enable LED based vibrator
    Enable LED based vibrator for sdm660 targets.
    
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- device.mk
+++ device.mk
@@ -434,6 +434,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
 PRODUCT_PACKAGES += \
