21d844ad6800248b47ed08109aeabd7d3c1d5014
Bharat Pawar <bpawar@codeaurora.org>
2021-01-19 19:48:05 +0530

    X00TD: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9

--- device.mk
+++ device.mk
@@ -434,11 +434,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
 PRODUCT_PACKAGES += \
