dfeb2b3b16d5b21410841e643ba27042eb36ba3b
Subinsmani <subins.mani@pixysos.com>
2021-09-12 09:38:24 +0530

    X00TD: Add makefiles for device tree
    Signed-off-by: Subinsmani <subins.mani@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from sdm660-common
--include device/asus/sdm660-common/BoardConfigCommon.mk
+BOARD_VENDOR := asus
 
 DEVICE_PATH := device/asus/X00TD
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Assert
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
@@ -21,6 +33,13 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := X00TD_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM := sdm660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
 
 # Inherit the proprietary files
 -include vendor/asus/X00TD/BoardConfigVendor.mk
