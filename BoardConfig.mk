77d8dc184c420d96bff4101715224e1189f4be90
Vivekachooz <vivekachooz@gmail.com>
2021-11-01 11:33:05 +0530

    X00TD: Enable BOARD_PLAT_PRIVATE_SEPOLICY_DIR
    Change-Id: Ib6c47991ccdaa378d59961a5c30ce6b2b11f1aab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Soong namespaces
