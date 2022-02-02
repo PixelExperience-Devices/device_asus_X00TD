ea6707aab46297771da19beda7aab9e97b6aea02
ZVNexus <akoskovich@pm.me>
2022-02-02 14:36:49 +0530

    X00TD: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: Ia2cb9394fdc65148100032620aabc306f7b3c1e7

--- device.mk
+++ device.mk
@@ -364,10 +364,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_asus
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
