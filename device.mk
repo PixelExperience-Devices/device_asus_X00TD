85149775ad9841312c43fdafe6f020c57c60506a
Vivekachooz <vivekachooz@gmail.com>
2022-02-05 17:31:26 +0530

    X00TD: Update Bluetooth blobs from ddv_sprout
    * From ddv_sprout-m690-user-11-RKQ1.210607.001-00WW_3_440-release-keys
    
    Change-Id: I14bf551f511cd5ffb20738ef524281187cf8a21c

--- device.mk
+++ device.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
