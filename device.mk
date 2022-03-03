300b86982502c10a72fef5418b1a089da92ec82f
LuK1337 <priv.luk@gmail.com>
2022-03-03 20:43:47 +0530

    X00TD: Disable APEX compression
    Kernel panics on linux-4.4 + ext4.
    
    Change-Id: I3ae1322880286a7edf8e53e6f602618d129bba91

--- device.mk
+++ device.mk
@@ -6,6 +6,7 @@
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+PRODUCT_COMPRESSED_APEX := false
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
