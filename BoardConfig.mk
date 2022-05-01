54e4c4cc4aad357f19a1be41e86cca0360f421bd
Anton Hansson <hansson@google.com>
2022-05-01 09:48:47 +0530

    X00TD: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a73
+TARGET_2ND_CPU_VARIANT := generic
 
 # Assert
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
