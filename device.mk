fbb3299c08d4d5a2e8a111dd26dc7927fb938e9a
Alin Jerpelea <alin.jerpelea@sony.com>
2022-06-13 07:37:17 +0530

    X00TD: Add qmi_fw.conf
    This file will silence the warnings on qmi
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Change-Id: I7d7ec0da00a33a0da8f189b0181d40919b9918e5

--- device.mk
+++ device.mk
@@ -354,6 +354,9 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
+
 # QNS
 PRODUCT_PACKAGES += \
     libstdc++.vendor
