2e16a23c6b98160b5074586f7ce94ea1f664c6cd
Rahul Krishna <Rahulkrishna585@gmail.com>
2022-06-20 09:51:07 +0530

    X00TD: Import initial overlays from LA.UM.8.2.r1-07100-sdm660.0
    * Remove unsupported overlays
    
    Change-Id: I03377d773c7226d9421eb675b63a8c88ba369c84

--- device.mk
+++ device.mk
@@ -24,6 +24,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
