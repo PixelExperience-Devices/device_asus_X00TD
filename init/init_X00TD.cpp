44e5bc8ddcd88d430cd245b16c6b0ec702c8c911
Vivekachooz <vivekachooz@gmail.com>
2022-07-05 20:56:35 +0530

    X00TD: Don't set dalvik props according to total ram
    Change-Id: I4bd87d420354051d587938de7c5431f7be602546

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -44,9 +44,6 @@
 using android::base::GetProperty;
 using std::string;
 
-string heapstartsize, heapgrowthlimit, heapsize,
-       heapminfree, heapmaxfree, heaptargetutilization;
-
 void property_override(string prop, string value)
 {
     auto pi = (prop_info*) __system_property_find(prop.c_str());
@@ -55,39 +52,6 @@ void property_override(string prop, string value)
         __system_property_update(pi, value.c_str(), value.size());
     else
         __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
-}
-
-void check_device()
-{
-    struct sysinfo sys;
-
-    sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
-    }
 }
 
 void NFC_check()
@@ -106,13 +70,5 @@ void NFC_check()
 
 void vendor_load_properties()
 {
-    check_device();
     NFC_check();
-
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
