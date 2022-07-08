e323694162d185280307c69de6b0fe60a0326b98
Vivekachooz <vivekachooz@gmail.com>
2022-07-08 20:48:32 +0530

    X00TD: Build custom fingerprint HIDL
    Change-Id: Ic26083b221d852434073f8be5e8263627f76cec7

--- device.mk
+++ device.mk
@@ -138,8 +138,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.1-service.X00TD
 
 # FM
 PRODUCT_PACKAGES += \
