33e7c9c6d4f8ecc290cfb2b49e11b5c40cea0722
Jabiyeff <cebiyevanar@gmail.com>
2022-08-12 20:35:38 +0530

    X00TD: Force disable iorapd
    * Its eating more RAM and slowdown device
    * Thanks for CAFEBABE find this
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Change-Id: I1b263a200a41da53bbe3f76bcec6e252499ab240

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,7 +121,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
