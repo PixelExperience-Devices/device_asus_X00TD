8e40aaa6fe6f49a0ddc54811f103d60261ca69bb
Vivekachooz <vivekachooz@gmail.com>
2023-02-17 19:16:15 +0530

    X00TD: Switch back to old amplifier logic
    Change-Id: Icd2984a8542db92bb6679164915968b908a51bef

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -18,27 +18,37 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := \
-    audio.primary.$(TARGET_BOARD_PLATFORM) \
-    libaudioroute \
-    libaudioutils \
-    libcutils \
+    libbase \
     liblog \
+    libcutils \
     libtinyalsa \
-    libtinycompress
+    libtinycompress \
+    libaudioroute \
+    libdl \
+    libaudioutils \
+    libhwbinder \
+    libhidlbase \
+    libprocessgroup \
+    libutils \
+    audio.primary.sdm660
 
 LOCAL_C_INCLUDES += \
-    $(call include-path-for,audio-route) \
+    external/tinyalsa/include \
+    external/tinycompress/include \
+    hardware/libhardware/include \
+    system/media/audio_utils/include \
+    $(call include-path-for, audio-effects) \
+    $(call include-path-for, audio-route) \
     $(call project-path-for,qcom-audio)/hal \
     $(call project-path-for,qcom-audio)/hal/msm8974 \
-    $(call project-path-for,qcom-audio)/hal/audio_extn
+    $(call project-path-for,qcom-audio)/hal/audio_extn \
+    $(call project-path-for,qcom-audio)/hal/voice_extn
 
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := audio_amplifier.c
-LOCAL_MODULE := audio_amplifier.$(TARGET_BOARD_PLATFORM)
-LOCAL_HEADER_LIBRARIES := \
-    generated_kernel_headers \
-    libhardware_headers
-
+LOCAL_MODULE := audio_amplifier.sdm660
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
