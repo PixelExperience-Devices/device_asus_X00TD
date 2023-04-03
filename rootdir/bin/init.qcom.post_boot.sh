58e9da6350f839d3b86d3865ed80a7e4c6bbab1e
Vivekachooz <vivekachooz@gmail.com>
2023-04-03 12:16:39 +0530

    X00TD: Set swappiness to a lower value of 30
    * Setting swappiness to 100 cause aggressive swap writing which
      further leads to unusual lags
    
    Change-Id: I8b62ee32579105591ccf9036b17b03e1b2c88774

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -769,9 +769,9 @@ else
     fi
 
     # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
+    # Set swappiness to 30 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    echo 30 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
