4e6d75411f167332732520b3e9035887ace78127
Vivekachooz <vivekachooz@gmail.com>
2023-06-20 12:56:42 +0530

    X00TD: Update graphics blobs from cat S62Pro
    * From sdm660_64-user 11 RKQ1.210406.002 1.015.00 release-keys
    
    Change-Id: Ia8b2ac034f42ee444ca881a150852625d27d2829

--- device.mk
+++ device.mk
@@ -99,19 +99,24 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libgralloc.qti \
     libtinyxml \
     libqdMetaData \
     libqdMetaData.system
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.frameworks.displayservice@1.0 \
-    android.frameworks.displayservice@1.0.vendor
+    android.frameworks.displayservice@1.0.vendor \
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # Doze mode
 PRODUCT_PACKAGES += \
