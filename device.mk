ef0f7c55ed4299cfec70977f9172cf5b06c4d770
Bruno Martins <bgcngm@gmail.com>
2023-06-21 12:56:42 +0530

    X00TD: Create power init script
     * Gather all performance related items in one place
       and drop all deprecated services.
    
    Change-Id: I4dbdc164c60136b0957a8c1ff67feafbaf471747

--- device.mk
+++ device.mk
@@ -375,6 +375,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.asus.rc \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
     init.target.rc \
