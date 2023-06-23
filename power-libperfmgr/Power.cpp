4f22cfe17e232efb1c0fc8c3b17c78e20a17f0e2
Wei Wang <wvw@google.com>
2023-06-23 12:56:42 +0530

    X00TD: power-libperfmgr: Fix objects that are derived from refbase
    Fixes: ag/14313466
    Bug: 177493042
    Bug: 191163855
    Test: Build
    Change-Id: I94812997a8214b77a2e1d0bcf90ef62205c5adf6
    Signed-off-by: Wei Wang <wvw@google.com>

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -87,7 +87,7 @@ Power::Power(std::shared_ptr<HintManager> hm)
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(DEBUG) << "Power setMode: " << toString(type) << " to: " << enabled;
     ATRACE_INT(toString(type).c_str(), enabled);
-    PowerSessionManager::getInstance().updateHintMode(toString(type), enabled);
+    PowerSessionManager::getInstance()->updateHintMode(toString(type), enabled);
     switch (type) {
 #ifdef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
