592f4108ff399b2700e3de8f962a12db8ddd9abe
LuK1337 <priv.luk@gmail.com>
2023-06-27 12:56:42 +0530

    X00TD: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 
-TARGET_KERNEL_CONFIG := vendor/X00TD_defconfig
+TARGET_KERNEL_CONFIG := vendor/X00TD_defconfig vendor/debugfs.config
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
 TARGET_KERNEL_VERSION := 4.19
 
